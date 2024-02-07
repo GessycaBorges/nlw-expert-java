836b9b35ed3935c7e548119c541c91b4a66c4e57
Gessyca Borges <gessyca.borges@gmail.com>
2024-02-07 11:49:52 -0300

    Primeiro Post, teste

--- src/main/java/com/rocketseat/certification_nlw/PrimeiraController.java
+++ src/main/java/com/rocketseat/certification_nlw/PrimeiraController.java
@@ -3,14 +3,24 @@ package com.rocketseat.certification_nlw;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
     @GetMapping("/retornarPrimeiraController")
-    public String retornoPrimeiraController(){
-        return "Criando minha primeira controller";
+    public Usuario retornoPrimeiraController(){
+        var usuario = new Usuario("Gessyca", 31);
+        return usuario;
+    }
+
+    @PostMapping("/meuPrimeiroPost")
+    public String primeiroPost() {
+        return "Meu primeiro POST";
+    }
+
+    record Usuario (String nome, int idade){
     }
 
 }
